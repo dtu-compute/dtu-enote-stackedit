f53f73497c12af215e4b7a67c599288e40802b3a
dtu-compute <dtu-compute@users.noreply.github.com>
2016-12-25 13:19:56 +0100

    Now using local couchdb:5984

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/bash
 
-
-# db is the hostname of the couchdb host which is specified in docker-compose from linking containers
-DB=couchdb
-
 while true; do
-    if curl  --insecure -k https://$DB:6984 | /usr/bin/grep -q Welcome; then
+    if curl couchdb:5984 | /usr/bin/grep -q Welcome; then
         echo "Database up"
         break
     fi
@@ -13,10 +9,8 @@ while true; do
     sleep 5
 done
 
-
 # ignore self signed cert warning
 export NODE_TLS_REJECT_UNAUTHORIZED=0
-
 
 echo create databases/documents...
 node config-dtu.js
@@ -26,4 +20,3 @@ export SESSION_SECRET=$(od -vAn -N16 -tx < /dev/urandom | tr -d " ")
 
 echo starting server...
 node server.js
-
