ff97b6fc6db443c5db42ff8543dcd189ffb6a848
Iain Bryson <bryson.iain@gmail.com>
2017-02-03 03:45:30 +0000

    Add better logging; remove automatic upload of documents until/unless it works.

--- config-dtu.js
+++ config-dtu.js
@@ -2,7 +2,7 @@ var yamlJs = require('yamljs');
 var __ = require('underscore');
 var url = require('url');
 var runsync = require('runsync'); // use this instead of child_process since we're bound to node 0.10.x
-var uploadAllDocs = require("./upload-all-docs.js");
+//var uploadAllDocs = require("./upload-all-docs.js");
 
 
 function run_cmd(cmd, args) {
@@ -42,9 +42,9 @@ try {
   var docker_admin_url = couchdbInfo._admin.db.replace(db_admin_url.host, docker_host).replace('https', 'http');
 
   console.log("USERS/DBS BEFORE SETUP");
-  var get_users_args = ["--insecure", docker_admin_url + '/_users/_all_docs', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity'];
-  var get_admins_args = ["--insecure", docker_admin_url + '/_config/admins', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity'];
-  var get_dbs_args = ["--insecure", docker_admin_url + '/_all_dbs', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity'];
+  var get_users_args = ["--insecure", "-sS", docker_admin_url + '/_users/_all_docs', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity'];
+  var get_admins_args = ["--insecure", "-sS", docker_admin_url + '/_config/admins', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity'];
+  var get_dbs_args = ["--insecure", "-sS", docker_admin_url + '/_all_dbs', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity'];
   run_cmd("curl", get_users_args);
   run_cmd("curl", get_admins_args);
   run_cmd("curl", get_dbs_args);
@@ -56,13 +56,13 @@ try {
         password = up[1];
 
       var docker_url = info.db.replace(db_url.host, docker_host).replace('https', 'http');
-      var docker_url_comps = url.parse(docker_url);
+      //var docker_url_comps = url.parse(docker_url);
 
       db_url.host = docker_host;
 
       console.log("checking to see if db/user exists for " + course + " ...");
 
-      var args = ["--insecure", docker_url + '/_design/by_update/_view/default?descending=true&include_docs=true&limit=25&reduce=false', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity',
+      var args = ["--insecure", "-sS", docker_url + '/_design/by_update/_view/default?descending=true&include_docs=true&limit=25&reduce=false', '-H', 'Accept: application/json, text/javascript; q=0.01', '-H', 'Accept-Encoding: identity',
         //'-H', Authorization: Basic dXNlcjAxMDA1OnBhc3MwMTAwNQ=='
       ];
       var result = run_cmd("curl", args);
@@ -73,19 +73,19 @@ try {
         // TODO :we probably should be creating users here, not admins as we're doing. http://wiki.apache.org/couchdb/How_to_create_users_via_script
         console.log("creating the user " + username + "...");
 
-        args = ["--insecure", "-X", "PUT", docker_admin_url + "/_config/admins/" + username, "-d", '"' + password + '"'];
+        args = ["--insecure", "-sS", "-X", "PUT", docker_admin_url + "/_config/admins/" + username, "-d", '"' + password + '"'];
         run_cmd("curl", args);
 
         console.log("creating the db...");
-        args = ["--insecure", "-X", "PUT", docker_url];
+        args = ["--insecure", "-sS", "-X", "PUT", docker_url];
         run_cmd("curl", args);
 
         console.log("configuring the db...");
         args = ["couchdb/setup.js", docker_url];
         run_cmd("node", args);
 
-        console.log("populating existing files...");
-        uploadAllDocs(docker_url.replace(docker_url_comps.pathname, ""), db_url.path.replace("/", ""), couchdbInfoRootPath + '../website-raw/' + course);
+        //console.log("populating existing files...");
+        //uploadAllDocs(docker_url.replace(docker_url_comps.pathname, ""), db_url.path.replace("/", ""), couchdbInfoRootPath + '../website-raw/' + course);
 
         args = ["couchdb/setup.js", docker_url];
         run_cmd("node", args);
